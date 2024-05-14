a213a21f128249df986fad9c437de2f6e485e5b2
bialger <artur.bigulov@yandex.ru>
2024-05-14 13:01:14 +0300

    Fixed include statements-2

--- libraries/argparser/ArgParser.hpp
+++ libraries/argparser/ArgParser.hpp
@@ -14,7 +14,7 @@ inline auto highLevelF(Args&&... args) -> decltype(lowLevelF(std::forward<Args>(
 
 #include "ConcreteArgumentBuilder.hpp"
 #include "ConcreteArgument.hpp"
-#include "libraries/argparser/basic/BasicFunctions.hpp"
+#include "./basic/BasicFunctions.hpp"
 
 namespace ArgumentParser {
 
