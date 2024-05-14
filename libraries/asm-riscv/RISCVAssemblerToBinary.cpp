95257a174a24be8c9e9103a4e1e4ce3df9d9431f
bialger <artur.bigulov@yandex.ru>
2024-05-14 07:59:12 +0300

    Added Assembler interface

--- libraries/asm-riscv/RISCVAssemblerToBinary.cpp
+++ libraries/asm-riscv/RISCVAssemblerToBinary.cpp
@@ -1,1 +1,15 @@
 #include "RISCVAssemblerToBinary.hpp"
+
+uint32_t RISCVAssemblerToBinary::ToBinary(const RISCVAssemblerCommand& command) {
+  return 0;
+}
+
+std::vector<uint32_t> RISCVAssemblerToBinary::Assemble(const std::vector<RISCVAssemblerCommand>& commands) {
+  std::vector<uint32_t> result(commands.size());
+
+  for (size_t i = 0; i < commands.size(); i++) {
+    result[i] = ToBinary(commands[i]);
+  }
+
+  return result;
+}
--- libraries/asm-riscv/RISCVAssemblerToBinary.hpp
+++ libraries/asm-riscv/RISCVAssemblerToBinary.hpp
@@ -1,8 +1,19 @@
 #ifndef LIBRARIES_ASM_RISCV_RISCVASSEMBLERTOBINARY_HPP_
 #define LIBRARIES_ASM_RISCV_RISCVASSEMBLERTOBINARY_HPP_
 
-class RISCVAssemblerToBinary {
+#include <vector>
+#include <cstdint>
 
+#include "RISCVAssemblerCommand.hpp"
+
+class RISCVAssemblerToBinary {
+ public:
+  RISCVAssemblerToBinary() = default;
+
+  static std::vector<uint32_t> Assemble(const std::vector<RISCVAssemblerCommand>& commands);
+
+ private:
+  static uint32_t ToBinary(const RISCVAssemblerCommand& command);
 };
 
 #endif //LIBRARIES_ASM_RISCV_RISCVASSEMBLERTOBINARY_HPP_
