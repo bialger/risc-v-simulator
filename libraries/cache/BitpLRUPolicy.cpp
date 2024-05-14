ed68f4bf8fed0889c400a51dbb39cfd819e52b67
bialger <artur.bigulov@yandex.ru>
2024-05-14 07:30:19 +0300

    Implemented cache policies

--- libraries/cache/BitpLRUPolicy.cpp
+++ libraries/cache/BitpLRUPolicy.cpp
@@ -1,1 +1,34 @@
 #include "BitpLRUPolicy.hpp"
+
+BitpLRUPolicy::BitpLRUPolicy(std::vector<std::vector<int>>& cache)
+    : cache_(cache), has_cache_replacements_(CACHE_SETS, std::vector<bool>(CACHE_WAY, false)) {
+  for (auto& line : cache_) {
+    line.resize(CACHE_WAY);
+
+    for (int32_t& word : line) {
+      word = 0;
+    }
+  }
+}
+
+void BitpLRUPolicy::UpdatePolicy(size_t index, size_t id) {
+  has_cache_replacements_[index][id] = true;
+
+  for (size_t i = 0; i < has_cache_replacements_[index].size(); ++i) {
+    if (!has_cache_replacements_[index][i]) {
+      return;
+    }
+  }
+
+  for (size_t i = 0; i < has_cache_replacements_[index].size(); ++i) {
+    if (i != id) {
+      has_cache_replacements_[index][i] = false;
+    }
+  }
+}
+
+void BitpLRUPolicy::AddLine(size_t index, int32_t tag) {
+  size_t id = Find(has_cache_replacements_[index], false);
+  cache_[index][id] = tag;
+  UpdatePolicy(index, id);
+}
--- libraries/cache/LRUPolicy.cpp
+++ libraries/cache/LRUPolicy.cpp
@@ -1,1 +1,17 @@
 #include "LRUPolicy.hpp"
+
+LRUPolicy::LRUPolicy(std::vector<std::vector<int32_t>>& cache) : cache_(cache) {}
+
+void LRUPolicy::UpdatePolicy(size_t index, size_t id) {
+  int32_t tmp_tag = cache_[index][id];
+  cache_[index].erase(cache_[index].begin() + static_cast<int64_t>(id));
+  cache_[index].push_back(tmp_tag);
+}
+
+void LRUPolicy::AddLine(size_t index, int32_t tag) {
+  if (cache_[index].size() == CACHE_WAY) {
+    cache_[index].erase(cache_[index].begin());
+  }
+
+  cache_[index].push_back(tag);
+}
