1592168c4ef074b31ffef00ddc9b6396a10bd85c
bialger <artur.bigulov@yandex.ru>
2024-05-14 07:29:30 +0300

    Added declarations of policies

--- libraries/cache/BitpLRUPolicy.hpp
+++ libraries/cache/BitpLRUPolicy.hpp
@@ -1,8 +1,24 @@
 #ifndef LIBRARIES_CACHE_BITPLRUPOLICY_HPP_
 #define LIBRARIES_CACHE_BITPLRUPOLICY_HPP_
 
-class BitpLRUPolicy {
+#include <vector>
 
+#include "constants.hpp"
+#include "common_functions.hpp"
+
+class BitpLRUPolicy {
+ public:
+  BitpLRUPolicy() = delete;
+  explicit BitpLRUPolicy(std::vector<std::vector<int32_t>>& cache);
+
+  void UpdatePolicy(size_t index, size_t id);
+  void AddLine(size_t index, int32_t tag);
+
+ private:
+  std::vector<std::vector<bool>> has_cache_replacements_;
+  std::vector<std::vector<int32_t>>& cache_;
 };
+
+static_assert(CachePolicy<BitpLRUPolicy>);
 
 #endif //LIBRARIES_CACHE_BITPLRUPOLICY_HPP_
--- libraries/cache/LRUPolicy.hpp
+++ libraries/cache/LRUPolicy.hpp
@@ -1,8 +1,23 @@
 #ifndef LIBRARIES_CACHE_LRUPOLICY_HPP_
 #define LIBRARIES_CACHE_LRUPOLICY_HPP_
 
-class LRUPolicy {
+#include <vector>
 
+#include "constants.hpp"
+#include "common_functions.hpp"
+
+class LRUPolicy {
+ public:
+  LRUPolicy() = delete;
+  explicit LRUPolicy(std::vector<std::vector<int32_t>>& cache);
+
+  void UpdatePolicy(size_t index, size_t id);
+  void AddLine(size_t index, int32_t tag);
+
+ private:
+  std::vector<std::vector<int32_t>>& cache_;
 };
+
+static_assert(CachePolicy<LRUPolicy>);
 
 #endif //LIBRARIES_CACHE_LRUPOLICY_HPP_
